d3e16280f2aeeb0b3218ec834fab3f6be9aacd81
dibulidohu <dibulidohu@blackfish.cn>
2019-08-16 10:31:53 +0800

    erueka

--- art-web/src/main/java/com/art/artweb/ArtWebApplication.java
+++ art-web/src/main/java/com/art/artweb/ArtWebApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.concurrent.Semaphore;
+
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.art")
 public class ArtWebApplication {
